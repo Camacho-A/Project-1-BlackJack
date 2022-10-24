496c7da58f7f8327479cde66d28437b2142e22c1
Angelica Camacho <camachoangelicaa.com>
2022-10-24 14:06:32 -0400

    update to API links and buttons

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const baseURL = "https://deckofcardsapi.com/api/deck"
-const newDeck = "/new/shuffle/?deck_count=6"
+const baseURL = "https://deckofcardsapi.com/"
+
+
 const gameSpecs = {
 	aces: "low",
 	scoreToWin: 21,
@@ -8,21 +9,82 @@ const gameSpecs = {
 		Jack: 10,
 		Queen: 10,
 		King: 10,
-    }
+	},
+
+}
+// Draw two cards, plus passes the deck_id in the parameters deck_ID
+function dealCards(deck_ID) {
+	const deckRequestURL = `${baseURL}api/deck/${deck_ID}/draw/?count=2`
+	$.ajax(deckRequestURL)
+		.then((data) => {
+			const draw = data.cards
+			console.log(draw)
+		},
+		(error) => {
+			console.log("An error occurred")
+		}
+	)
+	
+}
+
+// this function will generate a new deck with the count of 6(shuffled) with a new deck id 
+function newDecks() {
+	const newDeckURL = `${baseURL}api/deck/new/shuffle/?deck_count=6`
+	$.ajax(newDeckURL)
+		.then((data) => {
+			console.log(data)
+			const $playerOneHand = $(".player-one-hand")
+			const $computerHand = $(".computer-hand")
+
+			const deck_ID = data.deck_id
+
+			dealCards(deck_ID)
+		},
+		(error) => {
+			console.log("An error occurred")
+		}
+	)
 }
 
 
-function decks () {
-    const url = `${baseURL}${newDeck}`
-    console.log(url)
-         
-}
+// create click for deal
+const dealButton = document.getElementById("deal")
+dealButton.addEventListener("click", function () {
+	if (
+		$("#player-one-score").html() < gameSpecs.scoreToWin &&
+		$("#computer-score").html() < gameSpecs.scoreToWin
+	) 
+	console.log("it works")
+})
+
+// create click for reset. this also resets the score
+const resetButton = document.getElementById("reset")
+resetButton.addEventListener("click", function () {
+	$(".player-one-score").html(0)
+	$(".computer-score").html(0)
+	$("#result").html("")
+
+	console.log("reset works")
+})
 
 
-document.getElementById('button').onclick = function() {
-   alert("button was clicked");
-}​;​
-decks()
+
+
+
+
+
+
+
+
+newDecks()
+
+
+
+
+
+
+
+
 
 
 
